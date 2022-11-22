7b54e80d84bed24fcdd3767ba6d109afad3a55c3
usmanawan7 <usmanahmadawan7@gmail.com>
2022-11-22 23:11:04 +0500

    My portfolio

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# usman-portfolio
+## 📋 <a name="table">Table of Contents</a>
+
+1. 🤖 [Introduction](#introduction)
+2. ⚙️ [Tech Stack](#tech-stack)
+3. 🔋 [Features](#features)
+4. 🤸 [Quick Start](#quick-start)
+5. 🕸️ [Code to Copy](#snippets)
+6. 🔗 [Assets](#links)
+7. 🚀 [More](#more)
---
+++ data/index.ts
@@ -0,0 +1,207 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Healthcare UI Design",
+    des: "A healthcare platform that connects patients with doctors, pharmacists, and insurance providers.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://health-care-management-pied-two.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "8Returns",
+    des: "8Returns is the returns platform that turns customers into loyal fans, while saving you time and money. It provide the third party service to shopify stores.",
+    img: "/p2.svg",
+    iconLists: ["/ruby.svg", "/next.svg", "/tail.svg", "/ts.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Stridy",
+    des: "Stridy is building a community of volunteers who are passionate about clean cities by organising clean ups, workshops and talks.",
+    img: "/stridy.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Usman was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Usman's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Usman is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Usman was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Usman's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Usman is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Usman was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Usman's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Usman is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Usman was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Usman's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Usman is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Usman was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Usman's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Usman is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Associate Software Engineer",
+    desc: "Assisted in the development of a web-based platform using Ruby on Rails, React.js Shopify, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Developer",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Senior Software Engineer",
+    desc: "Developed and maintained user-facing features using modern technologies. Like Ruby on Rails, Astro, React, Nest.js , Node.js.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    url: "https://github.com/usmanawan7",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    url: "https://www.linkedin.com/in/usmanahmadawan/",
+  },
+];
